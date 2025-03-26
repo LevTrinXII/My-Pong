8b1b63ef7a7e12ca80cf8640de11f5e97880a9b2
LevTrinXII <aryehloeb@gmail.com>
2025-03-26 19:00:28 +0100

    Add files via upload

---
+++ My Pong/src/scenes/pong.js
@@ -0,0 +1,19 @@
+// "Every great game begins with a single scene. Let's make this one unforgettable!"
+export class Pong extends Phaser.Scene {
+    constructor() {
+        super('Pong');
+    }
+
+    init() {
+        // Initialize scene
+    }
+
+    preload() {
+        // Load assets
+    }
+
+    create() {
+        // Create game objects
+    }
+
+}
